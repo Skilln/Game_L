91b8eb281bdfc19ca8278eb9205b50c7e90c3230
Danylo Heraimovych <Skilln.mine@gmail.com>
2019-01-31 21:12:02 +0200

    Project created

---
+++ android/src/com/skilln/gamel/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.skilln.gamel;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.skilln.gamel.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/skilln/gamel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skilln.gamel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skilln.gamel.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
